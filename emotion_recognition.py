7f97a065a6a16470011f2b43808a0c8fce92bff1
Lane Hogan <lane.hogan1@gmail.com>
2022-11-27 09:57:08 -0500

    Reformat code

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -6,6 +6,9 @@ The faces have been automatically registered so that the face is more or less ce
 and occupies about the same amount of space in each image
 '''
 
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,23 +21,23 @@ print("The size of the dataset is: ", df.shape)
 df = df.sample(frac=0.1, random_state=46)
 print("The size of the dataset is: ", df.shape)
 
-
-
 ''' Extract images and label from the dataframe df '''
 width, height = 48, 48
 images = df['pixels'].tolist()
 faces = []
 for sample in images:
-    face = [int(pixel) for pixel in sample.split(' ')]  # Splitting the string by space character as a list
-    face = np.asarray(face).reshape(width*height)       # convert pixels to images and # Resizing the image
+    # Splitting the string by space character as a list
+    face = [int(pixel) for pixel in sample.split(' ')]
+    # convert pixels to images and # Resizing the image
+    face = np.asarray(face).reshape(width*height)
     faces.append(face.astype('float32') / 255.0)       # Normalization
 faces = np.asarray(faces)
 
 # Get labels
 y = df['emotion'].values
 
-
-class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+class_names = ['Angry', 'Disgust', 'Fear',
+               'Happy', 'Sad', 'Surprise', 'Neutral']
 # Visualization a few sample images
 plt.figure(figsize=(5, 5))
 for i in range(6):
@@ -46,23 +49,18 @@ for i in range(6):
     plt.xlabel(class_names[y[i]])
 plt.show()
 
-
-## Split data into training and test sets
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(faces, y, test_size=0.1, random_state=46)
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    faces, y, test_size=0.1, random_state=46)
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-
-from sklearn.svm import SVC
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train, y_train)
-
 
 # Now that our classifier has been trained, let's make predictions on the test data. To make predictions, the predict method of the DecisionTreeClassifier class is used.
 y_pred = svclassifier.predict(X_test)
 
 # For classification tasks some commonly used metrics are confusion matrix, precision, recall, and F1 score.
 # These are calculated by using sklearn's metrics library contains the classification_report and confusion_matrix methods
-from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
