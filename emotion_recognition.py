9094834129a2a88bd4876e4ebc231a2529887ca9
Lane Hogan <lane.hogan1@gmail.com>
2022-12-05 16:23:15 -0500

    Cleanup

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -6,28 +6,24 @@ The faces have been automatically registered so that the face is more or less ce
 and occupies about the same amount of space in each image
 '''
 
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import metrics
+from sklearn import metrics, tree
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
-
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 ''' ### Read csv data '''
-
-df = pd.read_csv('datasets\\fer2013\\train.csv')
+df = pd.read_csv('dataset/train.csv')
 print("There are total ", len(df), " sample in the loaded dataset.")
 print("The size of the dataset is: ", df.shape)
 # get a subset of the whole data for now
 df = df.sample(frac=0.1, random_state=46)
 print("The size of the dataset is: ", df.shape)
 
-### Extract images and label from the dataframe df
+# Extract images and label from the dataframe df
 width, height = 48, 48
 images = df['pixels'].tolist()
 faces = []
@@ -35,7 +31,7 @@ for sample in images:
     # Splitting the string by space character as a list
     face = [int(pixel) for pixel in sample.split(' ')]
     # convert pixels to images and # Resizing the image
-    face = np.asarray(face).reshape(width*height)
+    face = np.asarray(face).reshape(width * height)
     faces.append(face.astype('float32') / 255.0)       # Normalization
 faces = np.asarray(faces)
 
@@ -71,28 +67,29 @@ y_pred = svclassifier.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
-
-
-''' Decision Tree '''
-emotions_dataset = pd.read_csv('datasets\\fer2013\\normalizedData-collectedData-sorted.csv')
-print("\n\n\nThere are total ", len(emotions_dataset), " sample in the loaded dataset.")
+# Decision Tree
+emotions_dataset = pd.read_csv(
+    'dataset/normalizedData-collectedData-sorted.csv')
+print(
+    f"\n\n\nThere are total {len(emotions_dataset)} sample in the loaded dataset.")
 # print(emotions_dataset)
-
-X = emotions_dataset.iloc[:,:-1].values
-Y = emotions_dataset.iloc[:,10]
+X = emotions_dataset.iloc[:, :-1].values
+Y = emotions_dataset.iloc[:, 10]
 
 print(X)
 print(Y)
 
-clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = DecisionTreeClassifier(criterion='entropy')
 clf = clf.fit(X, Y)
 
 prediction = clf.predict([
     [0.2312, 0.67535, 0.34, 0, 0.1122, 0.514453, 0.1221, 0.8643, 0.323, 0.367613],
     [0.2, 0, 0.34, 0, 0.232, 0.9, 0.234, 0.86, 0.435, 0.787],
-    [0.9475984379, 1, 0.058230485348, 0.999, 0.223341, 0.57634, 0.1111, 0.8654, 0, 0.24],
+    [0.9475984379, 1, 0.058230485348, 0.999,
+        0.223341, 0.57634, 0.1111, 0.8654, 0, 0.24],
     [0, 1, 0, 0, 0, 0, 0.9999, 0.4543, 1, 0.23423],
-    [0.3645645, 0.2223, 0.34323, 0.235521, 0.2452134, 0.256542, 0.45616, 0.49765, 0.4162, 0.6007],
+    [0.3645645, 0.2223, 0.34323, 0.235521, 0.2452134,
+        0.256542, 0.45616, 0.49765, 0.4162, 0.6007],
 ])
 
 print(f"Prediction: {prediction}")
@@ -100,7 +97,8 @@ print(f"Prediction: {prediction}")
 tree.plot_tree(clf)
 plt.show()
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=30)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.1, random_state=30)
 
 clf.fit(X_train, Y_train)
 predicted = clf.predict(X_test)
@@ -108,5 +106,3 @@ predicted = clf.predict(X_test)
 print("\n", predicted)
 print(len(predicted))
 print("DecisionTrees's Accuracy: ", metrics.accuracy_score(Y_test, predicted))
-
-
